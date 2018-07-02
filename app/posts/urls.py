7d5b22c1a0bd1798f26b065f33517745759bbb84
LeeHanYeong <dev@lhy.kr>
2018-07-02 11:24:00 +0900

    post_like

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('tags/<str:tag>/', views.search_post_list, name='search-post-list'),
     path('<int:pk>/', views.post_detail, name='post-detail'),
     path('<int:pk>/delete/', views.post_delete, name='post-delete'),
+    path('<int:pk>/like/', views.post_like, name='post-like'),
+    path('<int:pk>/dislike/', views.post_dislike, name='post-dislike'),
     path('create/', views.post_create, name='post-create'),
 ]
--- app/posts/views/post_like.py
+++ app/posts/views/post_like.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
 from ..models import Post
+
+__all__ = (
+    'post_like',
+    'post_dislike',
+    'post_like_toggle',
+)
+
+
+@login_required
+@require_POST
+def post_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    request.user.like_posts.add(post)
+    return redirect('posts:post-detail', pk=pk)
+
+
+@login_required
+@require_POST
+def post_dislike(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    request.user.like_posts.remove(post)
+    return redirect('posts:post-detail', pk=pk)
+
+
+@login_required
+@require_POST
+def post_like_toggle(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user.like_posts.filter(post=post).exists():
+        request.user.like_posts.remove(post)
+    else:
+        request.user.like_posts.add(post)
+    return redirect('posts:post-detail', pk=pk)
